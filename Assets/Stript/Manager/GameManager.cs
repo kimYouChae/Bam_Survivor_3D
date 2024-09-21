dd59bc8dee7479109389b3afb109a359b8c19930
김지현 <kjhstar2486@naver.com>
2024-09-21 16:20:36 +0900

    콜라이더로 gridMap 저장 구현 완

--- Assets/Stript/Manager/GameManager.cs
+++ Assets/Stript/Manager/GameManager.cs
@@ -16,16 +16,37 @@ public class GameManager : MonoBehaviour
     [Header("===Eenemy Generator Time===")]
     [SerializeField] private float _unitGenerateTime;
 
-    // ÇÁ·ÎÆÛÆ¼
+    [Header("===LayerMask===")]
+    [SerializeField] private int _mapPropsLayer;
+
+    [Header("===GridMap===")]
+    [SerializeField] private bool[ , ] _mapGrid;
+    private int _mapSize = 140;
+    
+    //프로퍼티
     public float BasicRatio => _basicRatio;
     public float CommonRatio => _commonRatio;
     public float RareRatio => _rareRatio;
     public float EpicRatio => _epicRatio;
     public float LegaryRatio => _legendaryRatio;
     public float unitGenerateTime => _unitGenerateTime;
+    public int mapPropsLayer => _mapPropsLayer;
+    public bool[,] mapGrid => _mapGrid;
+    public int mapSize => _mapSize;
 
     private void Awake()
     {
         instance = this;
+
+        _mapPropsLayer = LayerMask.NameToLayer("MapProps");
+
+        // 기본값 false , 방문 시 true 
+        _mapGrid = new bool[_mapSize + 1 ,_mapSize + 1];
     }
+
+    public void F_SetGridMap(int v_x , int v_z , bool v_flag) 
+    {
+        _mapGrid[v_x , v_z] = v_flag;
+    }
+    
 }
